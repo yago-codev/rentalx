e0ea6523c4bb6455e15a8f49739b7d9d741cafbc
Yago Milano <yago.goncalves@invillia.com>
2022-03-05 20:32:35 -0300

    criando o repository de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,27 @@
 import { Router } from 'express';
 
-const categoriesRoutes = Router();
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
-const categories = [];
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
-    name,
-    description,
-  });
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-  return response.status(201).json(categories);
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: 'Category already exists!' });
+  }
+
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+});
+
+categoriesRoutes.get('/', (request, response) => {
+  const allCategories = categoriesRepository.list();
+  return response.status(200).json({ allCategories });
 });
 
 export { categoriesRoutes };
